231e334b0e8559d2c41c7921086fd5a97fd65aa4
SukitaLover <luccabrasilr@gmail.com>
2025-03-27 22:25:13 -0300

    Update main.cpp
    Mandando a parte 4 do projeto junto com a main do Arthur, ainda não está 100% por causa do resultado em float.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,45 @@ int prioridade(char operador) {
     }
 }
 
+// Resolução parte 4 projeto
+int avaliarExpressaoPosfixa(char *posfixa, int valores[]) {
+    Stack *pilha = CriaStack(50);
+    
+    for (int i = 0; posfixa[i] != '\0'; i++) {
+        char c = posfixa[i];
+        
+        if (c >= 'A' && c <= 'Z') {
+            //Empilha o valor da variavel correspondente
+            push(pilha, valores[c - 'A']);
+        } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+            // Desempilha os dois últimos valores
+            int b = pop(pilha);
+            int a = pop(pilha);
+            int resultado;
+            
+            //switch das operações do projeto
+            switch (c) {
+                case '+': resultado = a + b; break;
+                case '-': resultado = a - b; break;
+                case '*': resultado = a * b; break;
+                case '/': resultado = (b != 0) ? a / b : 0; break;
+                case '^': {
+                    resultado = 1;
+                    for (int j = 0; j < b; j++) resultado *= a;
+                    break;
+                }
+            }
+            
+            // Empilha o resultado
+            push(pilha, resultado);
+        }
+    }
+    
+    int resultadoFinal = pop(pilha);
+    DestroiStack(pilha);
+    return resultadoFinal;
+}
+
 int main() {
     char *texto = (char *)malloc(100 * sizeof(char));
     if (!texto) {
@@ -46,35 +85,26 @@ int main() {
     for (int i = 0; texto[i] != '\0' && texto[i] != '\n'; i++) {
         char c = texto[i];
 
-        // Se for uma variável (A-Z), vai direto para a saída
         if (c >= 'A' && c <= 'Z') {
             saida[j++] = c; 
-        } 
-        // Se for um operador, verificar prioridade
-        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+        } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
             while (!isEmptyChar(pilhaop) && prioridade(topoChar(pilhaop)) >= prioridade(c)) {
                 saida[j++] = popChar(pilhaop);
             }
             pushChar(pilhaop, c);
-        } 
-        // Se for um parêntese esquerdo, empilha
-        else if (c == '(') {
+        } else if (c == '(') {
             pushChar(pilhaop, c);
-        }
-        // Se for um parêntese direito, desempilha até encontrar o '('
-        else if (c == ')') {
+        } else if (c == ')') {
             while (!isEmptyChar(pilhaop) && topoChar(pilhaop) != '(') {
                 saida[j++] = popChar(pilhaop);
             }
-            popChar(pilhaop); // Remove o '('
-        }
-        else if (c != ' ') { 
+            popChar(pilhaop);
+        } else if (c != ' ') { 
             printf("Operação ou nome de variável inválidos!\n");
             return 0;
         }
     }
 
-    // Esvaziar a pilha no final
     while (!isEmptyChar(pilhaop)) {
         saida[j++] = popChar(pilhaop);
     }
@@ -82,38 +112,32 @@ int main() {
     saida[j] = '\0'; 
     printf("Expressão Pós-Fixa: %s\n", saida);
 
-     
-    //declaração do vetor de valores numericos
-    int valores[26]= {0};
-    //vetor usado para checar se o usuario preencheu as variaveis
-    bool valores_preenchidos[26]= {false};
+    int valores[26] = {0};
+    bool valores_preenchidos[26] = {false};
     
-    for (int i=0;saida[i]!='\0'; i++ ){
-        char c= saida[i];
-        if (c >= 'A' && c <= 'Z'){
-            //solitita o valor apenas uma vez, evitando assim repetiçao 
-            if (!valores_preenchidos[c-'A']){
-                printf("digite um valor para cada variável por favor %c:",c);
-                scanf("%d", &valores[c- 'A']);
-                //verifica se os valores foram devidamente preenchidos
-                valores_preenchidos[c-'A']= true;
-                
+    for (int i = 0; saida[i] != '\0'; i++) {
+        char c = saida[i];
+        if (c >= 'A' && c <= 'Z') {
+            if (!valores_preenchidos[c - 'A']) {
+                printf("Digite um valor para %c: ", c);
+                scanf("%d", &valores[c - 'A']);
+                valores_preenchidos[c - 'A'] = true;
             }
         }    
-        
     }
-    printf("os valores fornecidoos foram respectivamente:\n");
-    for (int i= 0; i<26; i++){
-    //verifica se o usuario forneceu valores
-    if (valores_preenchidos[i]){
-        printf("%c:%d\n", 'A'+i, valores[i]);
+
+    printf("Os valores fornecidos foram:\n");
+    for (int i = 0; i < 26; i++) {
+        if (valores_preenchidos[i]) {
+            printf("%c: %d\n", 'A' + i, valores[i]);
+        }
     }
     
-    }
-    // Liberar memória
+    int resultado = avaliarExpressaoPosfixa(saida, valores);
+    printf("Resultado da expressão: %d\n", resultado);
+    
     free(texto);
     free(saida);
     DestroiStackChar(pilhaop);
-
     return 0;
 }
